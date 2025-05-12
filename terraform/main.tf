ce8898b4f89a208169654049f7ce72fb9f211925
Sanjay Bhangar <sanjay@developmentseed.org>
2025-05-12 15:02:40 +0530

    try fixing passing down of eoapi_db_password

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,5 +2,6 @@ module "resources" {
     source = "./resources"
     
     environment = var.environment
-    aws_region      = var.aws_region
+    aws_region = var.aws_region
+    eoapi_db_password = var.eoapi_db_password
 }
