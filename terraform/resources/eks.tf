2889546fbb5d93b392dea33a843bd4246e3fcce8
Sanjay Bhangar <sanjay@developmentseed.org>
2025-05-14 11:31:00 +0530

    try changing desired size before min size

--- terraform/resources/eks.tf
+++ terraform/resources/eks.tf
@@ -100,9 +100,9 @@ module "eks" {
 
       instance_types = ["t3.large"]
 
-      min_size     = 2
+      min_size     = 1
       max_size     = 3
-      desired_size = 3
+      desired_size = 2
 
       # Launch template configuration
       use_custom_launch_template = true
