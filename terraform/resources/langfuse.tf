5af177a55f5a3efaeb3b624a1bf256c1bfe32f20
Sanjay Bhangar <sanjay@developmentseed.org>
2025-06-25 16:31:58 +0530

    add s3 buckets via terraform, new user with scoped access

---
+++ terraform/resources/langfuse.tf
@@ -0,0 +1,49 @@
+# S3 Bucket for current environment
+resource "aws_s3_bucket" "langfuse" {
+  bucket = "langfuse-${var.environment}-bucket"
+}
+
+# IAM user for Langfuse
+resource "aws_iam_user" "langfuse" {
+  name = "langfuse-${var.environment}-s3-user"
+}
+
+# Bucket policy
+resource "aws_iam_user_policy" "langfuse" {
+  name = "langfuse-${var.environment}-policy"
+  user = aws_iam_user.langfuse.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:DeleteObject",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          aws_s3_bucket.langfuse.arn,
+          "${aws_s3_bucket.langfuse.arn}/*"
+        ]
+      }
+    ]
+  })
+}
+
+# Access key for the IAM user
+resource "aws_iam_access_key" "langfuse" {
+  user = aws_iam_user.langfuse.name
+}
+
+# Outputs
+output "langfuse_access_key_id" {
+  value = aws_iam_access_key.langfuse.id
+}
+
+output "langfuse_secret_key" {
+  value     = aws_iam_access_key.langfuse.secret
+  sensitive = true
+}
