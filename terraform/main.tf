7c122613605bbf84f63efc83e9a24da7f33186ec
Sanjay Bhangar <sanjay@developmentseed.org>
2025-08-29 17:29:34 +0530

    try fixing output of role arn

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,3 +6,9 @@ module "resources" {
     eoapi_db_password = var.eoapi_db_password
 }
 
+# Expose cluster autoscaler IAM role ARN from resources module
+output "cluster_autoscaler_iam_role_arn" {
+  description = "ARN of the IAM role for cluster autoscaler"
+  value       = module.resources.cluster_autoscaler_iam_role_arn
+}
+
